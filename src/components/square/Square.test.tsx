303877dd8b2c4e9a9520f1285e0fa7b04d8bca9a
Dominik Depta <dominikdepta@gmail.com>
2025-01-20 16:29:26 +0100

    set up tests

---
+++ src/components/square/Square.test.tsx
@@ -0,0 +1,12 @@
+import { describe, expect, test } from "vitest";
+import { render, screen } from "@testing-library/react"
+import "@testing-library/jest-dom";
+import { Square } from "./Square";
+
+describe('<Square />', () => {
+  test("rendering button with given content", () => {
+    render(<Square>test content</Square>)
+
+    expect(screen.getByRole('button')).toHaveTextContent('test content');
+  })
+})
